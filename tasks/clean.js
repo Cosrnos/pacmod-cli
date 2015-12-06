2c5b26855e4188856c7f09bc91fe1fa65204683b
cosrnos <alex@coschedule.com>
2015-12-06 09:45:04 -0600

    [BUGFIX] dist-clean targeting wrong directory
    Signed-off-by: cosrnos <alex@coschedule.com>

--- tasks/clean.js
+++ tasks/clean.js
@@ -2,7 +2,7 @@ var del = require('del');
 
 module.exports = function (gulp, Config) {
     gulp.task('clean-build', clean_directory(Config.BUILD_DEST));
-    gulp.task('clean-dist', ['clean-build'], clean_directory(Config.DIST_FOLDER));
+    gulp.task('clean-dist', ['clean-build'], clean_directory(Config.DIST_PATH));
     gulp.task('bundle-commonjs-clean', ['clean-dist'], clean_directory(Config.BUILD_DEST + '/commonjs'));
 };
 
